b94c33efb447d0301c57a5a41566f18006f16d74
Adu Williams <aduwillie@gmail.com>
2020-02-11 13:55:38 +0000

    Added health route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const Home = require('./home');
 const SendEmail = require('./emails/send-email');
+const Health = require('./health');
 
 module.exports = [
     Home,
     SendEmail,
+    Health,
 ];
